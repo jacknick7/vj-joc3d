7f77493a42ee357f63bbef233d6c0708dc369144
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-01 22:11:13 +0100

    Menú

---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMain : MonoBehaviour {
+
+    public int LevelOne = 1;
+
+	public void StartLevelOne() {
+        SceneManager.LoadScene(LevelOne);
+    }
+}
