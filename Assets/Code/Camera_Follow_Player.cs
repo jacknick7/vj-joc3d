3d823fb85fccd7321ab8df3c1cc1fa0263b5b508
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-12-01 20:12:52 +0100

    primer nivell v0.5

---
+++ Assets/Code/Camera_Follow_Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow_Player : MonoBehaviour {
+
+    Transform PlayerTrasnsform;
+
+    Vector3 relativeDistancePlayer;
+    Vector3 oldPositionPlayer;
+
+
+    // Use this for initialization
+    void Start () {
+        PlayerTrasnsform = GameObject.Find("Player").transform;
+        if (PlayerTrasnsform == null) Debug.LogError("Error: No player found.");
+        else {
+            relativeDistancePlayer = PlayerTrasnsform.position - this.transform.position;
+            oldPositionPlayer = PlayerTrasnsform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerTrasnsform == null) Debug.LogError("Error: No player found.");
+        else if (oldPositionPlayer != PlayerTrasnsform.position) {
+            this.transform.position = PlayerTrasnsform.position - relativeDistancePlayer;
+            oldPositionPlayer = PlayerTrasnsform.position;
+        }
+	}
+}
---
+++ Assets/Code/Cube_Move_Rotate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Move_Rotate : MonoBehaviour {
+
+    [SerializeField] Vector3 movement = new Vector3(0.01f, 0.0f, 0.0f);
+
+    [SerializeField] float rotateX = 0.2f;
+    [SerializeField] float rotateY = 1.0f;
+    [SerializeField] float rotateZ = 2.5f;
+
+    float scale = 1.0f;
+    [SerializeField] float scaleIncrement = 0.001f;
+
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] bool useSpeed = false;
+
+    [SerializeField] Space transformSpace = Space.World;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = Time.deltaTime;
+        Move();
+        Rotate();
+        Scale();
+        scale += scaleIncrement;
+	}
+
+    public void Move() {
+        Vector3 translation = movement;
+        if (useSpeed) translation *= speed;
+        transform.Translate(translation, transformSpace);
+    }
+
+    public void Rotate() {
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ), transformSpace);
+    }
+
+    public void Scale() {
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
