aca8a185c006c9d99f679913f173f36b0c9ca26d
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-12-03 00:49:14 +0100

    logic_level v0.5 + time 1.0

---
+++ Assets/Code/Level_Logic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Logic : MonoBehaviour {
+
+    [SerializeField] int max_rutes = 9;
+    int actual_rute;
+    [SerializeField] float time = 60.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        //max_rutes = 9;
+        actual_rute = 0;
+        //time = 60.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        if (actual_rute > max_rutes)
+            SceneManager.LoadScene(0);  // tots els recorreguts complerts, carrega nou lvl o guanya
+        if (time < 0.0f)
+            SceneManager.LoadScene(0);  // no temps, perd
+
+        
+    }
+
+    public void incTime(float extra_time) {
+        Debug.Log(extra_time);
+        time += extra_time;
+    }
+}
---
+++ Assets/Code/Time_Object.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_Object : MonoBehaviour {
+
+    [SerializeField] float time_increment = 10.0f;
+    [SerializeField] string player_name;
+    [SerializeField] GameObject logic;
+    bool enter = false;
+
+    private void Start() {
+        player_name = GameObject.Find("MainPlayer").name;
+        logic = GameObject.Find("Level Logic");
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == player_name && !enter) {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            logic.GetComponent<Level_Logic>().incTime(time_increment);
+            enter = true;
+        }   
+    }
+
+    public void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == player_name) {
+            Destroy(this.gameObject);
+        }
+    }
+}
