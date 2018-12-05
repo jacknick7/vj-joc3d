d7d19af3ef53b115467d28143d0954e2aad5d480
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-12-06 00:50:36 +0100

    LogicLevel v1.0 & MapLimits v1.0

---
+++ Assets/Scripts/MapLimits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLimits : MonoBehaviour {
+
+    GameObject logic;
+
+    void Start() {
+        logic = GameObject.Find("Level Logic");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        string current_player_name = logic.GetComponent<LevelLogic>().getCurrentPlayerName();
+        if (other.gameObject.name == current_player_name) {
+            other.gameObject.GetComponent<CarController>().setCarStatus(0);
+            other.gameObject.GetComponent<CarController>().setCarStatus(1);
+        }
+    }
+}
