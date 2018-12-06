b3667af7d3ab85e7aba9e9ff0fbe4df316185253
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-06 16:17:29 +0100

    Coez de l'arrow

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        GameObject vehicle = GameObject.Find("Level Logic").GetComponent<LevelLogic>().getCurrentCar();
+        GameObject destination = GameObject.Find("Level Logic").GetComponent<LevelLogic>().getCurrentDestination();
+
+        Vector3 pos1 = vehicle.transform.position;
+        Vector3 pos2 = destination.transform.position;
+
+        float opo = pos2.x - pos1.x;
+        float con = pos2.z - pos1.z;
+
+        float angle = - Mathf.Atan(opo/con) * Mathf.Rad2Deg;
+
+        if(pos1.z > pos2.z){
+            if (pos1.x > pos2.x){
+                angle += 180.0f;
+            }
+            else{
+                angle -= 180.0f;
+            }
+        }
+
+        print("DATA");
+        print(opo);
+        print(con);
+        print(angle);
+
+        transform.Rotate(new Vector3(0, 0, angle - transform.eulerAngles.z));
+    }
 }
