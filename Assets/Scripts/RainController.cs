bd7f7a6e2fc567312f0e4b490880f09f0332da7a
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-08 00:39:16 +0100

    PLUJA!

---
+++ Assets/Scripts/RainController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour {
+    public bool raining = false;
+
+    public void setRaning(bool actRaining){
+        raining = actRaining;
+    }
+
+    void FixedUpdate(){
+        transform.GetChild(0).gameObject.SetActive(raining);
+    }
+}
