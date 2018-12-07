5d1ae22d1e489aafff072d5064e409b26a846caa
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-08 00:05:32 +0100

    Fer de nit

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+    public bool day = true;
+
+	public void setDay(bool actDay){
+        day = actDay;
+    }
+
+    void FixedUpdate(){
+        transform.GetChild(0).gameObject.SetActive(day);
+        transform.GetChild(1).gameObject.SetActive(!day);
+    }
+}
