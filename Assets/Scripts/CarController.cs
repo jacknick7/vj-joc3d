1685f99184364ec6960817bbd8fc59f44840da59
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-08 02:18:22 +0100

    Temps

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -125,9 +125,9 @@ public class CarController : MonoBehaviour {
                                                //BR.transform.position = BRv;
                                                //BR.transform.rotation = BRq;
 
-        if ((Mathf.Abs(Vector3.Dot(transform.up, Vector3.down)) < 0.125f) && (carStatus == 1)){
+        /*if ((Mathf.Abs(Vector3.Dot(transform.up, Vector3.down)) < 0.125f) && (carStatus == 1)){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        }*/
 
     }
 
