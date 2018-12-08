ce944d6e57f5f4779e1863f8167efe54452e7438
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-08 02:14:20 +0100

    Temps

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeController : MonoBehaviour {
+	void Update(){
+        float time = GameObject.Find("Level Logic").GetComponent<LevelLogic>().getTime();
+        int minutes = Mathf.RoundToInt(Mathf.Floor(time/60.0f));
+        int seconds = Mathf.RoundToInt(time-minutes*60);
+        string timeShow = minutes + ":";
+        if(seconds >= 60){
+            timeShow = (minutes + 1) + ":00";
+        }
+        else if(seconds < 10){
+            timeShow += "0" + seconds;
+        }
+        else{
+            timeShow += seconds;
+        }
+        GetComponent<TextMeshProUGUI>().SetText(timeShow);
+    }
+}
