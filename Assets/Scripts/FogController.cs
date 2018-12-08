320833f2e0e3a008aea9764b810798ecb71c777a
Oriol Closa <oriol.closa@est.fib.upc.edu>
2018-12-08 01:23:13 +0100

    BOIRA!

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour{
+    public bool fog = false;
+
+    public void setRaning(bool actFog){
+        fog = actFog;
+    }
+
+    void FixedUpdate(){
+        transform.GetChild(0).gameObject.SetActive(fog);
+    }
+}
